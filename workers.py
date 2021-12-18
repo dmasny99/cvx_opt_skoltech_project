1adef308f794ac2340b0ec52d59c284484453905
Dmitrii Masnyi <Dmitrii.Masnyi@skoltech.ru>
2021-12-19 02:59:21 +0300

    errors fixed

--- workers.py
+++ workers.py
@@ -3,7 +3,7 @@ import multiprocessing as mp
 
 def z_count(wk, k, df, L, delta, noisy_img):
     wk += ((k + 1) / 2) * df
-    zk = wk / max(L, np.linalg.norm(wk) / delta) + noisy_img
+    zk = -wk / max(L, np.linalg.norm(wk) / delta) + noisy_img
     return wk, zk
 
 def y_count(L, x, noisy_img, df, delta):
@@ -18,13 +18,12 @@ def gradient_count(df: mp.Value, pobj: mp.Value, i, j, m, x, mu):
     uij[0] = x[i1] - x[i3]
     uij[1] = x[i2] - x[i3]
     
-    c1 = np.sqrt(uij[0] * uij[0] + uij[1] * uij[1])
+    c1 = np.linalg.norm(uij)
     pobj += c1
     
     c2 = max(mu, c1)
-    uij[0] = uij[0] / c2
-    uij[1] = uij[1] / c2
-    
+    uij= uij / c2
+        
     df[i1] += uij[0]
     df[i3] -= uij[0]
     df[i2] += uij[1]
